f2a49906c88271c8b81e687b748b4b2cce448773
Renaud <Renaud@Renaud>
2014-10-26 01:28:59 +0200

    u

--- laby/src/main/java/com/renaud/laby/player/RenderWall3DEx.java
+++ laby/src/main/java/com/renaud/laby/player/RenderWall3DEx.java
@@ -3,7 +3,6 @@ package com.renaud.laby.player;
 import java.awt.Color;
 
 import com.renaud.laby.Labyrinthe;
-import com.renaud.laby.player.RenderWall3D.Point3D;
 import com.renaud.laby.view.IDrawOperation;
 
 public class RenderWall3DEx {
@@ -18,13 +17,16 @@ public class RenderWall3DEx {
 		}
 	}
 
-	private final static int largeur_mur = 80;
+	private final static int largeur_mur = 50;
 	private final static int longueur_mur = 50;
-	private final static int hauteur_mur = 100;
+	private final static int hauteur_mur = 140;
+	
+	private final static double limx = 150;
+	private final static double limy = 60;
 	
 	
 	private int sx = 230;
-	private int sy = 130;
+	private int sy = 150;
 
 	private Labyrinthe laby;
 	private Player player;
@@ -45,7 +47,7 @@ public class RenderWall3DEx {
 
 
 	public void render(IDrawOperation op) {
-		int dist = 4;
+		int dist = 5;
 		
 		for(int i=dist-1;i>=0;i--){
 			int pos = nextPos(i);
@@ -58,16 +60,23 @@ public class RenderWall3DEx {
 				boolean h = (kind & Player.HAUT_DIR) == Player.HAUT_DIR;
 			
 				this.drawWAll(Color.yellow, op, GROUND, 0, 0, longueur_mur * i);
+				this.drawWAll(Color.gray, op, GROUND, 0, hauteur_mur, longueur_mur * i);
 				
 				if(!h)this.drawWAll(Color.red, op, WALL_HAUT, 0, 0, longueur_mur * (i+1));
-				if(!d) this.drawWAll(Color.red, op, WALL_DROITE, 0, 0, longueur_mur * i); 
-				else {
+				if(!d){
+					this.drawWAll(Color.red, op, WALL_DROITE, 0, 0, longueur_mur * i);
+					this.drawWAll(Color.red, op, WALL_HAUT, largeur_mur, 0, longueur_mur * i);
+				}else {
 					this.drawWAll(Color.red, op, WALL_HAUT, largeur_mur, 0, longueur_mur * (i+1));
+					this.drawWAll(Color.gray, op, GROUND, largeur_mur, hauteur_mur, longueur_mur * i);
 					this.drawWAll(Color.yellow, op, GROUND, largeur_mur, 0, longueur_mur * i);
 				}
-				if(!g) this.drawWAll(Color.red, op, WALL_GAUCHE, 0, 0, longueur_mur * i); 
-				else {
+				if(!g) {
+					this.drawWAll(Color.red, op, WALL_GAUCHE, 0, 0, longueur_mur * i); 
+					this.drawWAll(Color.red, op, WALL_HAUT, -largeur_mur, 0, longueur_mur * i);
+				}else {
 					this.drawWAll(Color.red, op, WALL_HAUT, -largeur_mur, 0, longueur_mur * (i+1));
+					this.drawWAll(Color.gray, op, GROUND, -largeur_mur, hauteur_mur, longueur_mur * i);
 					this.drawWAll(Color.yellow, op, GROUND, -largeur_mur, 0, longueur_mur * i);
 				}
 			
@@ -171,13 +180,11 @@ public class RenderWall3DEx {
 	}
 
 	private void checkPoint(Point3D p,int dist){
-		double limx = 120;
-		double limy = 40;
+		
 		double sx = 1.0;
 		double sy = 1.0;
 		
 		double z = Math.sqrt(p.z+ dist)*8;
-	
 		
 		double x = p.x * (1-1 / limx * z);
 		double y = (p.y-limy) / p.x * x + limy;
